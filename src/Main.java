c9c94b4f1755f7dd31311a89bbeae1b504513bf2
jegalhb <jegalhb@gmail.com>
2026-03-05 12:47:19 +0900

    성적추가

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,13 @@ public class Main {
         //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
         // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
         // 테스트가 너무많아 지웟
+        int score = 77;
+        String grade = switch (score/10){
+
+            case 7 -> "c";
+
+            default -> throw new IllegalStateException("Unexpected value: " + score);
+        };
         System.out.println("test");
         System.out.println("test");
         System.out.println("test");
